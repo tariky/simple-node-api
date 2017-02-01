ba65e3f77ee3cce559a52227842452b7e644ae9b
tariky <takibih@gmail.com>
2017-02-01 09:58:55 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+
+const api = require('./server/routes/api');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use('/api', api);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`API running on localhost:${port}`));
