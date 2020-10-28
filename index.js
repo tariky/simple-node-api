76126a8bbc2cbb5a4bee8ad7c0a568e1038c4162
Tarik Čaplja <takibih@gmail.com>
2020-10-28 15:43:01 +0100

    Update index.js
    Added fallback endpoint

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname, "dist/index.html"));
+  });
+
 const port = process.env.PORT || '3000';
 app.set('port', port);
 
